ce9c20552db215d46c1da7a47b9f49f278e4b6c9
Marcelo <marcelosm@gmail.com>
2013-06-07 09:51:51 -0700

    finalmente usável !!!

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,6 +17,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 DISQUS_SHORTNAME = 'marceloatstanford'
+GITHUB_URL = "https://github.com/marcelom/marceloatstanford"
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = False
@@ -25,7 +26,7 @@ RELATIVE_URLS = False
 FILES_TO_COPY = (('favicon.ico', 'favicon.ico'),)
 
 # Wed 24 Dec 2013, for example
-#DEFAULT_DATE_FORMAT = '%a %d %b %Y'
+# DEFAULT_DATE_FORMAT = '%a %d %b %Y'
 DEFAULT_DATE_FORMAT = '%d %b %Y'
 
 # Filename Metadata: YYYY-MM-DD-the-rest-before-the-dot-is-the-slug.md, for example
@@ -37,7 +38,7 @@ ARTICLE_DIR = ('posts')
 ARTICLE_EXCLUDES = ('pages', '.')
 
 # My theme ignores some templates, so here they are...
-DIRECT_TEMPLATES = ('index',) #Just index, nothing else...
+DIRECT_TEMPLATES = ('index',)  # Just index, nothing else...
 ARTICLE_URL = '%s/posts/{slug}.html' % SITEURL_SUFFIX
 ARTICLE_SAVE_AS = 'posts/{slug}.html'
 ARTICLE_LANG_SAVE_AS = False  # Dsiables LANG processing, all site is in English...
@@ -46,8 +47,8 @@ PAGE_SAVE_AS = 'pages/{slug}.html'
 PAGE_LANG_SAVE_AS = False
 AUTHOR_SAVE_AS = False
 CATEGORY_SAVE_AS = False
-#TAG_URL = '%s/tags/{slug}.html' % SITEURL_SUFFIX
-#TAG_SAVE_AS = 'tags/{slug}.html'
+# TAG_URL = '%s/tags/{slug}.html' % SITEURL_SUFFIX
+# TAG_SAVE_AS = 'tags/{slug}.html'
 TAG_SAVE_AS = False
 YEAR_ARCHIVE_SAVE_AS = False
 MONTH_ARCHIVE_SAVE_AS = False
